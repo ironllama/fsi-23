c72b1bd318a6eb96ffffe20091dd13e173d6d68f
Alex <alexoh@wcoding.com>
2024-02-01 17:01:35 +0900

    Day03

---
+++ day03/functions.js
@@ -0,0 +1,78 @@
+// // Create a function.
+// function doSomethingCool() {
+//     console.log("This is cool.");
+// }
+
+// // Call the function.
+// doSomethingCool();
+
+// Create a function.
+// const doSomethingElse = function () {
+//     console.log("This is something else.");
+// }
+
+// // Call the function.
+// doSomethingElse();
+
+// function doSomethingMore(one, two, three) {
+//     console.log("TWO:", two);
+// }
+// doSomethingMore();
+
+// function secondName(one, two, three) {
+//     console.log("SECOND:", two);
+// }
+// secondName("Nik", "Eden");
+
+// function doSomethingWeird(one) {
+//     console.log("Weird:", one);
+// }
+// doSomethingWeird("Peter", "Mary");
+
+// function someMath(one, two) {
+//     two = one * one;
+// }
+// let a = 5;
+// let b = 10;
+// someMath(a, b);
+// console.log("B:", b);
+
+// function someMoreMath(one) {
+//     one[0] = one[1] + one[2];
+// }
+// let nums = [8, 4, 6, 2];
+// someMoreMath(nums);
+// console.log("NUMS:", nums[0]);
+
+// function someOtherMath(one) {
+//     return one * 2;
+// }
+// let num = 3;
+// num = someOtherMath(num);
+// console.log("NUM:", num);
+
+// function someOtherMath(one) {
+//     return one * 2;
+// }
+// let num = 3;
+// someOtherMath(num);
+// console.log("NUM:", num);
+
+// function someOtherThing(one) {
+//     one.color = "red";
+// }
+
+// let fruit = {
+//     color: "blue",
+//     name: "blueberry"
+// }
+// someOtherThing(fruit);
+// console.log("FRUIT COLOR:", fruit.color);
+
+function someOtherThing(one) {
+    one = "red";
+}
+
+let fruit = "blue";
+someOtherThing(fruit);
+console.log("FRUIT COLOR:", fruit);
