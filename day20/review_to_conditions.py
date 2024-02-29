0be23c182cd9b816838ba3f3caea9e1d732d9ec7
Alex <alexoh@wcoding.com>
2024-02-29 12:34:43 +0900

    Day 20: Exercise: review_to_conditions

---
+++ day20/review_to_conditions.py
@@ -0,0 +1,28 @@
+# 1. Ask your user for a first name value using the input function and
+#     assign the result to a variable called first_name.
+
+#    b. Ask your user for a last name value using the input function and
+#        assign the result to a variable called last_name.
+
+#    c. Combine first_name and last_name into a single variable full_name
+#        and print out the length of the string contained in that variable.
+
+
+# 2. Ask your user for a greeting and store it in a variable called greeting.
+
+#    b. Ask your user for the number of times they want to repeat that
+#        greeting and store it in a variable called count.
+
+#    c. Print out whether or not the count is less than 100.
+
+#    d. Print out the greeting repeated count number of times.
+
+
+# 3. Ask your user for the lowest value in a range and store it in a
+#     variable called low.
+
+#    b. Ask your user for the highest value in a range and store it in
+#        a variable called high.
+
+#    c. Ask you user for a value, and print whether or not that value
+#        is in the range (inclusive).
