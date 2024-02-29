67d556170ad42a8dcc63bb5ed3b8c494a084e9a4
Alex <alexoh@wcoding.com>
2024-02-29 17:46:42 +0900

    Day20: End

--- day20/dictionaries_and_unpacking.py
+++ day20/dictionaries_and_unpacking.py
@@ -1,7 +1,11 @@
 # 1. Create a dictionary called ruby with "beautiful" set to True
+ruby = { "beautiful": True }
 
 # 2. Create a dictionary called shield with "sturdy" set to False
+shield = { "sturdy": False }
 
 # 3. Create a dictionary called ruby_shield by unpacking ruby and shield into it
+ruby_shield = {**ruby, **shield}
 
 # 4. Print out ruby_shield
+print("ruby_shield:", ruby_shield)
